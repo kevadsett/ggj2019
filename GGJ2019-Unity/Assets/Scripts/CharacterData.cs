fd7655b74f2ea5eb142fad1c0d9e115f5993657a
noobnewbier <peter789b@gmail.com>
2019-01-26 10:45:22 +0000

    baseline of scriptable data

---
+++ GGJ2019-Unity/Assets/Scripts/CharacterData.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "ScriptableObjects/CharacterData")]
+public class CharacterData : ScriptableObject{
+    [SerializeField] private string _name;
+    public string Name { get { return _name; } }
+    [SerializeField] private Sprite _sprite;
+    public Sprite Sprite { get { return _sprite; } }
+}
---
+++ GGJ2019-Unity/Assets/Scripts/Requirements.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "ScriptableObjects/Requirements")]
+public class Requirements : ScriptableObject
+{
+    [SerializeField]
+    [Range(0f, 1f)]
+    private float _wetness = 0f;
+    public float Wetness { get { return _wetness; } }
+
+    public float RequirementMet(float wetness)
+    {
+        return 0f;
+    }
+}
