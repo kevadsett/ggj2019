fbd1b22040b034d6b349f9621a82d8d170c81201
noobnewbier <peter789b@gmail.com>
2019-01-26 12:45:44 +0000

    more bare bones

--- GGJ2019-Unity/Assets/Scripts/ScriptableObjects/ReviewDialogData.cs
+++ GGJ2019-Unity/Assets/Scripts/ScriptableObjects/ReviewDialogData.cs
@@ -6,11 +6,13 @@ public struct Review
 {
     public string Dialog { get; private set; }
     public int Level { get; private set; }
+    public AudioClip AudioClip { get; private set; }
 
-    public Review(string dialog, int level) : this()
+    public Review(string dialog, int level, AudioClip audioClip) : this()
     {
         Dialog = dialog;
         Level = level;
+        AudioClip = audioClip;
     }
 
 }
@@ -18,15 +20,14 @@ public struct Review
 [CreateAssetMenu(menuName = "ScriptableObjects/DialogData")]
 public class ReviewDialogData : ScriptableObject
 {
-    [SerializeField]
-    private string[] _dialogues;
-
+    [SerializeField] private string[] _dialogues;
+    [SerializeField] private SpeechData _speechData;
     public Review GetReview(float satisfaction)
     {
         Debug.Assert(satisfaction <= 1f);
         Debug.Assert(_dialogues.Length <= GameConfig.MAX_REVIEW_GRADE);
 
         int level = (int)(satisfaction / (1f / GameConfig.MAX_REVIEW_GRADE));
-        return new Review(_dialogues[level], level);
+        return new Review(_dialogues[level], level, _speechData.GetAudioClip(satisfaction));
     }
 }
---
+++ GGJ2019-Unity/Assets/Scripts/ScriptableObjects/SpeechData.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "ScriptableObjects/SpeechData")]
+public class SpeechData : ScriptableObject
+{
+    [SerializeField] private AudioClip _happyClip;
+    [SerializeField] private AudioClip _sadClip;
+
+    public AudioClip GetAudioClip(float satisfaction)
+    {
+        //crude implemention that only has 2 audio clip, to be modified if need a better implementation
+        return satisfaction > 0.5 ? _happyClip : _sadClip;
+    }
+}
