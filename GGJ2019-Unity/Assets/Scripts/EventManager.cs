06ec205e05f4cc44c81f21f12c02c742bae121a6
noobnewbier <peter789b@gmail.com>
2019-01-26 12:05:16 +0000

    barebones for service locator and event manager

---
+++ GGJ2019-Unity/Assets/Scripts/EventManager.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventManager : MonoBehaviour {
+    //todo : fill with events
+
+    //todo : fill with method that push an event
+}
---
+++ GGJ2019-Unity/Assets/Scripts/ServiceLocator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ServiceLocator : MonoBehaviour {
+
+    public static ServiceLocator Instance { get; private set; }
+
+    public EventManager EventManager { get; private set; }
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    private void Start()
+    {
+        EventManager = FindObjectOfType<EventManager>();
+    }
+}
