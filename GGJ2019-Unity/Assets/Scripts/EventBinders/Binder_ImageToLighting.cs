04f3daae8c63c8b221e0a889b01e624087a3faed
gav <notoriousknave@gmail.com>
2019-01-26 15:43:44 +0000

    added event binder component so things can change sprite + image on a lighting event

---
+++ GGJ2019-Unity/Assets/Scripts/EventBinders/Binder_ImageToLighting.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent (typeof(Image))]
+public class Binder_ImageToLighting : MonoBehaviour {
+
+    const bool defaultIsLit = true;
+
+    [SerializeField] Sprite litSprite;
+    [SerializeField] Sprite offSprite;
+
+    Image boundImage;
+
+	void Awake () {
+        boundImage = GetComponent<Image>();
+        Apply(defaultIsLit);
+
+        EventManager.LightingChanged += Apply;
+	}
+	
+	void Apply (bool isLit) {
+        boundImage.sprite = isLit ? litSprite : offSprite;
+	}
+
+    void OnDestroy() {
+        EventManager.LightingChanged -= Apply;
+    }
+}
--- GGJ2019-Unity/Assets/Scripts/EventManager.cs
+++ GGJ2019-Unity/Assets/Scripts/EventManager.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EventManager : MonoBehaviour {
-    //todo : fill with events
 
-    //todo : fill with method that push an event
+    public static event System.Action<bool> LightingChanged;
+    public static void Call_LightingChanged (bool isOn) {
+        LightingChanged(isOn);
+    }
 }
